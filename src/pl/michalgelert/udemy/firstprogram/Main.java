a57de5ce1a120da7d4e24d1e46e54dd20305e940
upike <javadkpobieranie@gmail.com>
2018-09-23 13:54:01 +0200

    Initial commit

---
+++ src/pl/michalgelert/udemy/firstprogram/Main.java
@@ -0,0 +1,11 @@
+package pl.michalgelert.udemy.firstprogram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.printf("test");
+
+
+    }
+}
